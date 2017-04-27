950084a25151666cc6b7f5c1bf413e187a5f1fd1
dannielccw <dannielccw@gmail.com>
2017-04-27 16:14:52 +0800

    介紹如何測試自建的class的手法

---
+++ app/src/main/java/com/example/yvtc/s042703/StudentData.java
@@ -0,0 +1,12 @@
+package com.example.yvtc.s042703;
+
+/**
+ * Created by YVTC on 2017/4/27.
+ */
+
+public class StudentData {
+    public String getData()
+    {
+        return "Jack";
+    }
+}
---
+++ app/src/test/java/com/example/yvtc/s042703/MyTest1.java
@@ -0,0 +1,18 @@
+package com.example.yvtc.s042703;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by YVTC on 2017/4/27.
+ */
+
+public class MyTest1 {
+    @Test
+    public void MyTest1()
+    {
+        StudentData sd = new StudentData();
+        assertEquals(sd.getData(), "Jack");
+    }
+}
